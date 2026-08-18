d738a0b80e535d84be4aa48c9d53a798231d31b7
ujjwalmishracse <ujjwalmishra2022@gmail.com>
2026-08-18 16:14:51 +0530

    Add FSD SEM-3 files

---
+++ FSD-SEM-3/README.md
@@ -0,0 +1,1 @@
+RSGDGFHJKJ
---
+++ datatype.js
@@ -0,0 +1,64 @@
+let name = "UJJWAL";
+
+let age = 20;
+
+console.log(name);
+
+let student = {
+    name: "ujjwal",
+    age: 20
+};
+
+let x = 10;
+
+x++;
+
+console.log(x);
+
+x--;
+
+console.log(x);
+
+const n = {
+    name: "ujjwal",
+    age: 20
+};
+
+n.name = "nick";
+
+n.age = 21;
+
+console.log(n);
+
+console.log("hello"); //syntax error show krega 
+
+//console.log(name); //reference error show krega
+
+//const arr = new Array(-5); //range error show krega
+
+//decodeURL("%"); //URL error show krega
+
+function add(a,b) {
+    return a+b;
+}
+
+console.log(add(10,20));
+
+
+//arror function
+
+const add1 = (a,b) => {
+    return a+b;
+}
+
+console.log(add1(10,20));
+
+
+
+let f = ['apple', 'banana', 'orange'];
+
+f.push('grapes');
+
+f.pop();
+
+console.log(f);git 
